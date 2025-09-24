cae0eca3045bc41675c831728d778b12d36501b8
Ilyas <myjobil.ku@gmail.com>
2025-09-24 19:40:52 +0300

    [Feature] Add now Class + Add Model

--- DeepSeek Alamofire/Models/Model.swift
+++ DeepSeek Alamofire/Models/Model.swift
@@ -7,4 +7,35 @@
 
 import Foundation
 
+struct ChatCompletionRequest: Encodable {
+    let model: String
+    let messages: [RequestMessage]
+}
 
+struct RequestMessage: Encodable {
+    let role: String
+    let content: String
+}
+
+struct ChatCompletionResponse: Decodable {
+    let choices: [Choice]
+    
+    struct Choice: Decodable {
+        let message: ResponseMessage
+        
+        struct ResponseMessage: Decodable {
+            let content: String
+        }
+    }
+}
+
+//{
+//  "model": "deepseek/deepseek-r1-0528:free",
+//  "messages": [
+//    {
+//      "role": "user",
+//      "content": "What is the meaning of life?"
+//    }
+//  ]
+//  
+//}
---
+++ DeepSeek Alamofire/NetworkManager/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  DeepSeek Alamofire
+//
+//  Created by Ильяс on 24.09.2025.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManager {
+    
+    func sendALRequest() {
+        
+    }
+}
