9320676abe6b9a07d948d7ea4e34c41b2d29b451
Ilyas <myjobil.ku@gmail.com>
2025-09-24 20:58:13 +0300

    [Feature] Successful launch of the application with sending API data

--- DeepSeek Alamofire/ViewController.swift
+++ DeepSeek Alamofire/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let networkManager = NetworkManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        // Делаем пробный запрос
+        networkManager.sendALRequest(message: "Привет, расскажи шутку!") { choices in
+            for choice in choices {
+                print("Ответ AI: \(choice.message.content)")
+            }
+        }
     }
-
 
 }
 
