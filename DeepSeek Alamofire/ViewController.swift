df27910c6386a6e7f1813d6f997a920252302bbb
Ilyas <myjobil.ku@gmail.com>
2025-09-25 16:23:20 +0300

    [Feature] Added new API Model, network and successful request

--- DeepSeek Alamofire/ViewController.swift
+++ DeepSeek Alamofire/ViewController.swift
@@ -6,19 +6,35 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
+    
     let networkManager = NetworkManager()
+    
+    lazy var imageView: UIImageView = {
+        $0.frame.origin = .zero
+        $0.frame.size = view.frame.size
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFill
+        return $0
+    }(UIImageView())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(imageView)
         
         // Делаем пробный запрос
-        networkManager.sendALRequest(message: "Привет, расскажи шутку!") { choices in
+        networkManager.sendALRequest(message: "Привет") { choices in
             for choice in choices {
                 print("Ответ AI: \(choice.message.content)")
             }
+        }
+        
+        networkManager.getRandomPhoto { [weak self] in
+            guard let self = self else { return }
+            imageView.sd_setImage(with: URL(string: $0), placeholderImage: .actions)
         }
     }
 
