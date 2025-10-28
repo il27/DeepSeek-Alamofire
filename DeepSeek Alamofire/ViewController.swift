161bb9f25a0812c62408c272bb147a5e59a908ca
Ilyas <myjobil.ku@gmail.com>
2025-10-28 23:19:12 +0300

    [Feature] Add promptTextFilde

--- DeepSeek Alamofire/ViewController.swift
+++ DeepSeek Alamofire/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     
     let networkManager = NetworkManager()
     
+    lazy var promptTextFilde: UITextField = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.clipsToBounds = true
+        $0.placeholder = "Prompt"
+        $0.layer.borderWidth = 2
+        $0.layer.borderColor = UIColor.systemRed.cgColor
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        $0.leftViewMode = .always
+        return $0
+    }(UITextField())
+    
     lazy var imageView: UIImageView = {
         $0.frame.origin = .zero
         $0.frame.size = view.frame.size
@@ -24,6 +35,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(imageView)
+        setupUI()
         
         // Делаем пробный запрос
         networkManager.sendALRequest(message: "Привет") { choices in
@@ -36,6 +48,23 @@ class ViewController: UIViewController {
             guard let self = self else { return }
             imageView.sd_setImage(with: URL(string: $0), placeholderImage: .actions)
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        promptTextFilde.layer.cornerRadius = promptTextFilde.frame.height / 2
+//        buttonDeepSeek.layer.cornerRadius = buttonDeepSeek.frame.height / 2
+    }
+    
+    func setupUI() {
+        view.addSubview(promptTextFilde)
+        
+        NSLayoutConstraint.activate([
+            promptTextFilde.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            promptTextFilde.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            promptTextFilde.heightAnchor.constraint(equalToConstant: 54),
+            promptTextFilde.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -24),
+        ])
     }
 
 }
