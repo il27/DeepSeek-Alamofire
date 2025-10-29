39830aa059deb263e0e7a418cfb5e0390d57c041
Ilyas <myjobil.ku@gmail.com>
2025-10-29 23:15:56 +0300

    [Feature] add button viewController

--- DeepSeek Alamofire/ViewController.swift
+++ DeepSeek Alamofire/ViewController.swift
@@ -17,11 +17,20 @@ class ViewController: UIViewController {
         $0.clipsToBounds = true
         $0.placeholder = "Prompt"
         $0.layer.borderWidth = 2
-        $0.layer.borderColor = UIColor.systemRed.cgColor
+        $0.layer.borderColor = UIColor.systemGray.cgColor
         $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
         $0.leftViewMode = .always
         return $0
     }(UITextField())
+    
+    lazy var promptButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.clipsToBounds = true
+        $0.backgroundColor = .systemGray
+        $0.setTitle("→", for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        return $0
+    }(UIButton())
     
     lazy var imageView: UIImageView = {
         $0.frame.origin = .zero
@@ -53,17 +62,23 @@ class ViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         promptTextFilde.layer.cornerRadius = promptTextFilde.frame.height / 2
-//        buttonDeepSeek.layer.cornerRadius = buttonDeepSeek.frame.height / 2
+        promptButton.layer.cornerRadius = promptButton.frame.height / 2
     }
     
     func setupUI() {
         view.addSubview(promptTextFilde)
+        view.addSubview(promptButton)
         
         NSLayoutConstraint.activate([
             promptTextFilde.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            promptTextFilde.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            promptTextFilde.trailingAnchor.constraint(equalTo: promptButton.leadingAnchor, constant: -16),
             promptTextFilde.heightAnchor.constraint(equalToConstant: 54),
             promptTextFilde.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -24),
+            
+            promptButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            promptButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -24),
+            promptButton.heightAnchor.constraint(equalToConstant: 54),
+            promptButton.widthAnchor.constraint(equalToConstant: 54),
         ])
     }
 
